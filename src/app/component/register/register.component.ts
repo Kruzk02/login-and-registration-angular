f9c44246c16dd394498c2a27118c78ec3d51a4d1
thienphuc <thienphuc484@gmail.com>
2024-10-19 13:19:55 +0700

    Add Register components withou style

---
+++ src/app/component/register/register.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { RegisterDTO } from '../../dtos/RegisterDTO';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  imports: [HttpClientModule, ReactiveFormsModule,RouterLink,CommonModule],
+  providers: [AuthService]
+})
+
+export class RegisterComponent implements OnInit {
+  
+  registerDTO: RegisterDTO = { username: '', email: '', password: '' };
+  registerForm!: FormGroup;
+  message: string | undefined;
+  
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  register() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.registerDTO = this.registerForm.value;
+
+    this.authService.register(this.registerDTO).subscribe(response => {
+      if (response.status === 'ok') {
+        this.message = response.message;
+        console.log(response.message);
+      } else {
+        this.message = response.message;
+        console.error(response.message);
+      }
+    }, error => {
+      console.error('An error occurred while registering:', error);
+      this.message = 'An unexpected error occurred. Please try again.';
+    });    
+  }
+}
