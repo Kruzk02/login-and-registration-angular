db1a6cbd654fc3e585132cd0d74399ebc14d378b
thienphuc <thienphuc484@gmail.com>
2024-10-29 10:39:43 +0700

    Update Auth Service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,17 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { EventEmitter, Injectable, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, } from '@angular/core';
 import { RegisterDTO } from '../dtos/RegisterDTO';
-import { catchError, map, Observable, of } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of, throwError } from 'rxjs';
 import { LoginDTO } from '../dtos/LoginDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private loggedInSubject = new BehaviorSubject<boolean>(false);
+  public loggedIn = this.loggedInSubject.asObservable();
 
-  @Output() loggedIn: EventEmitter<boolean> = new EventEmitter();
-  @Output() username: EventEmitter<string> = new EventEmitter();
- 
+  private usernameSubject = new BehaviorSubject<string | null>(null);
+  public username = this.usernameSubject.asObservable();
+  
   constructor(private httpClient: HttpClient) { }
 
   register(registerDTO: RegisterDTO):Observable<any> {
@@ -28,21 +30,51 @@ export class AuthService {
       .pipe(map(response => {
         if (response.status === 'ok' && response.token) {
           sessionStorage.setItem("token", response.token);
+          this.loggedInSubject.next(true)
+
+          this.getUsername().subscribe(username => {
+            sessionStorage.setItem("username",username)
+            this.usernameSubject.next(username)
+          });
           return true;
         }
         return false;
       }),
-      catchError(error => {
-        this.loggedIn.emit(false);
+      catchError(() => {
+        this.loggedInSubject.next(false);
         return of(false);
       }))
   }
+  
+  getUsername(): Observable<string> {
+    const token = this.getJwtToken();
 
+    if (!token) {
+      return of("");
+    }
+    
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${token}`);
+    return this.httpClient.get<{username :string}>("http://localhost:8080/api/get-username", { headers }).pipe(
+      map(response => response.username),
+      catchError(error => {
+        console.error("Error fetching username:", error);
+        return of("");
+      })
+    );
+  }
+  
   getJwtToken() {
     return sessionStorage.getItem("token");
   }
 
   isLoggedIn() {
     return this.getJwtToken() != null;
+  }
+
+  logout(): void {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("username");
+    this.loggedInSubject.next(false);
+    this.usernameSubject.next(null);
   }
 }
