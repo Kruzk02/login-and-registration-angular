aebdbdc7538f2b397c9317ef22f55e83abee8349
thienphuc <thienphuc484@gmail.com>
2024-10-29 10:45:21 +0700

    Update Header component

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,15 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,CommonModule,HttpClientModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit {
+  username: string | null = null;
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.loggedIn.subscribe(status => {
+      this.isLoggedIn = status;
+      if (!status) {
+        this.username = null;
+      }
+    });
+
+    this.authService.username.subscribe(username => this.username = username);
+    
+    this.username = sessionStorage.getItem("username");
+    this.isLoggedIn = !!sessionStorage.getItem("token");
+  }
 }
