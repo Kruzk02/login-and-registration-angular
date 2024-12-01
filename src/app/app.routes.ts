d32719e795acafcfe75e6bea74d45a315e2820a6
thienphuc <thienphuc484@gmail.com>
2024-12-01 14:08:10 +0700

    feat(app.routes): add new path for verify account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
+import { TokenGuard } from './guards/token.guard';
+import { VerifyComponent } from './component/verify/verify.component';
 
 export const routes: Routes = [
     {path: 'login', canActivate: [AuthGuard], component: LoginComponent},
-    {path: 'register', canActivate: [AuthGuard], component: RegisterComponent}
+    {path: 'register', canActivate: [AuthGuard], component: RegisterComponent},
+    {path: 'verify',canActivate: [TokenGuard], component: VerifyComponent}
 ];
