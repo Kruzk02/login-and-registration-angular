598b90ca1356d03912425316ef2117d330b95e5a
Thien phuc <thienphuc484@gmail.com>
2025-03-14 11:58:57 +0700

    feat(config): add authInterceptor to providerHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+
+import { authInterceptor } from './interceptors/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
